9c3bed51f792c399762196da4ac16b0dd2b1ba0d
meenasriram98 <meena.sriram@imaginea.com>
2019-03-28 09:49:24 +0530

    second change

--- JavaTraining/src/Admin.java
+++
@@ -1,8 +0,0 @@
-
-public class Admin extends Person {
-public static int MIN_AGE_TO_DRINK=21;
-public static int MIN_AGE_TO_VOTE=21;
-public static int MIN_AGE_FOR_MALE_TO_GET_MARRIED=21;
-public static int MIN_AGE_FOR_FEMALE_TO_GET_MARRIED=21;
-
-}
